0b5e97685f7c9697c157b0a376dc64f8f78aebf0
JoydeepMallick <joydeep150703@gmail.com>
2025-04-01 23:33:51 +0530

    april fool 2025

---
+++ 1.4.25 April Fools Day Contest 2025/A.py
@@ -0,0 +1,2 @@
+# print("print(\"puzzling\")")
+print("puzzling")
---
+++ 1.4.25 April Fools Day Contest 2025/H.py
@@ -0,0 +1,17 @@
+dick = {
+    1 : "CODEFORCES",
+    2 : "EYE",
+    3 : "TESTING",
+    4 : "SYSTEM",
+    5 : "APRIL",
+    6 : "BLURRY",
+    7 : "BLURRY",
+    8 : "BLURRY",
+    9 : "BLURRY",
+    10 : "BLURRY",
+    11 : "BLURRY",
+}
+
+_coord = int(input())
+print(dick[_coord])
+
